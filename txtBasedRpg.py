636850bd967b5aeded06da8ad9ea99f50e11e037
Alexander Hargrave <alexanderhargrave@hotmail.co.uk>
2022-07-08 11:59:17 +0100

    Update txtBasedRpg.py

--- txtBasedRpg.py
+++ txtBasedRpg.py
@@ -1,30 +1,40 @@
 from random import randint
 
+
 class Base:
-    def __init__(self,species,level,hp,maxhp,atk,currentatk,defence,speed):
+    def __init__(self, species, level, hp, max_hp, atk, current_atk, defence, speed):
         self.species = species
         self.level = level
         self.hp = hp
-        self.maxhp = maxhp
+        self.max_hp = max_hp
         self.atk = atk
-        self.currentatk = currentatk
+        self.current_atk = current_atk
         self.defence = defence
         self.speed = speed
-    def getspecies(self):
+
+    def get_species(self):
         return self.species
-    def getlevel(self):
+
+    def get_level(self):
         return self.level
-    def gethp(self):
+
+    def get_hp(self):
         return self.hp
-    def getmaxhp(self):
-        return self.maxhp
-    def getatk(self):
+
+    def get_max_hp(self):
+        return self.max_hp
+
+    def get_atk(self):
         return self.atk
-    def getcurrentatk(self):
-        return self.currentatk
-    def getdefence(self):
+
+    def get_current_atk(self):
+        return self.current_atk
+
+    def get_defence(self):
         return self.defence
-    def getspeed(self):
+
+    def get_speed(self):
         return self.speed
-    def change_level(self,lvlchange):
-        self.level += lvlchange
+
+    def change_level(self, lvl_change):
+        self.level += lvl_change
